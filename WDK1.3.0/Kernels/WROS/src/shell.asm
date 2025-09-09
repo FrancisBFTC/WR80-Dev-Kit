32717abe9f90bf7c151c36aef11303893677e5b2
FrancisBFTC <bftcorporations@gmail.com>
2025-09-08 23:43:15 -0300

    feat: Add shell commands and interpret

--- WDK1.3.0/Kernels/WROS/src/shell.asm
+++ WDK1.3.0/Kernels/WROS/src/shell.asm
@@ -2,8 +2,71 @@ shell:
 	call configcli
 	
 .loop:
+	cdr
+	ld r0
 	call readkey
+	
+	call getbreak
+	jz interpret
+	
 	call writechar
 	jp .loop
 	
+interpret:
+	cdr
+	out p2
+	
+	call getcmdaddr
+	call saveregs
+	
+begin:
+	call getregs
+	call setaddr
+	in p2
+	pushd
+	
+	call incregs
+	call setaddr
+	in p2
+	pushd
+	
+	call incregs
+	call saveregs
+	
+	pop r0
+	pop r1
+	
+	call configbuff
+	call strcmp
+	jc begin
+	
+	
+	jp .loop
+	
+cmd.vec:
+	dw str.echo, str.ls, str.read
+	
+commands:
+	str.echo:
+		db "echo",0
+		dw cmd.echo
+	str.ls:
+		db "ls",0
+		dw cmd.ls
+	str.read:
+		db "read",0
+		dw cmd.read
+	
+cmd.echo:
+
+ret
+
+cmd.ls:
+
+ret
+
+cmd.read:
+
+ret
+
 include "kernel.inc"
