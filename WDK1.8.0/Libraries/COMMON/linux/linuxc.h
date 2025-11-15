6105afeb3ab89ab6f5d90c0c0d88ac48a22cad49
FrancisBFTC <bftcorporations@gmail.com>
2025-11-15 20:05:50 -0300

    upd: Linux change 3

--- WDK1.8.0/Libraries/COMMON/linux/linuxc.h
+++ WDK1.8.0/Libraries/COMMON/linux/linuxc.h
@@ -1,6 +1,8 @@
 #ifndef LINUXC_H
 #define LINUXC_H
 
+#define _POSIX_C_SOURCE 199309L
+
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
@@ -13,6 +15,7 @@
 #include <netdb.h>
 #include <pthread.h>
 #include <errno.h>
+#include <time.h>
 
 #define INVALID_SOCKET 	-1
 #define SOCKET_ERROR   	-1
