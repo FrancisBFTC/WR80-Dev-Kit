3a01a9de861a466958c68fd47712a2f1533781c2
FrancisBFTC <bftcorporations@gmail.com>
2025-11-16 01:24:39 -0300

    updates

--- WDK1.8.0/Libraries/COMMON/linux/linuxc.h
+++ WDK1.8.0/Libraries/COMMON/linux/linuxc.h
@@ -90,11 +90,11 @@ int _kbhit() {
     int nread;
 
     if (peek_character != -1) return 1;
-    new_settings.c_cc[VMIN]=0;
-    tcsetattr(0, TCSANOW, &new_settings);
+    //new_settings.c_cc[VMIN]=0;
+    //tcsetattr(0, TCSANOW, &new_settings);
     nread = read(0, &ch, 1);
-    new_settings.c_cc[VMIN]=1;
-    tcsetattr(0, TCSANOW, &new_settings);
+    //new_settings.c_cc[VMIN]=1;
+    //tcsetattr(0, TCSANOW, &new_settings);
 
     if (nread == 1) {
         peek_character = ch;
