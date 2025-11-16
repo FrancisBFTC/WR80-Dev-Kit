c83c069d85c9c849e196583d125be31ee014573b
FrancisBFTC <bftcorporations@gmail.com>
2025-11-16 01:20:17 -0300

    up

--- WDK1.8.0/Libraries/COMMON/linux/linuxc.h
+++ WDK1.8.0/Libraries/COMMON/linux/linuxc.h
@@ -89,7 +89,6 @@ int _kbhit() {
     char ch;
     int nread;
 
-	init_keyboard();
     if (peek_character != -1) return 1;
     new_settings.c_cc[VMIN]=0;
     tcsetattr(0, TCSANOW, &new_settings);
@@ -97,7 +96,6 @@ int _kbhit() {
     new_settings.c_cc[VMIN]=1;
     tcsetattr(0, TCSANOW, &new_settings);
 
-	reset_keyboard();
     if (nread == 1) {
         peek_character = ch;
         return 1;
