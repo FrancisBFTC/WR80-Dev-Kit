b70749e7ff29e3acc9dfaaa2bebf404cb46dd653
FrancisBFTC <bftcorporations@gmail.com>
2025-11-17 00:39:20 -0300

    Last organizations

--- build.sh
+++ build.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-git restore *
-git pull origin wdk1.8.0-compat
-cd WDK1.8.0/Emulators/WR80DBG
+echo Installing Assembler...
+cd WDK1.8.0/Assemblers/WR80ASM
 make install
-cd ../../Kernels/WROS
-./build.sh
-cd ../../../
+
+echo
+echo Installing Image Builder...
+cd ../WR80IMG
+make install
+
+echo
+echo Installing Emulator...
+cd ../../Emulators/WR80EMU
+make install
+
+echo
+echo Installing Debugger...
+cd ../WR80DBG
+make install
+
+echo
+echo Installing Virtual Machine...
+cd ../WR80VM
+make install
+
+echo
+echo All tools installed!
